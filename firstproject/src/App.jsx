fb13bfc032df272ec7af7ef858aaa48ba64e072f
Prashansa Shandilya <prashansashandilya428@gmail.com>
2026-03-17 07:26:20 +0000

    ok

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -5,18 +5,24 @@ import './App.css'
 import Demo from './components/Demo'
 
 function App() {
+  const name="PRASH";
+  let x=10;
+  let y=20;
+  function PP() {
+    return "inside function";
+  }
+  function sum(a,b){
+    return a + b;
+  }
  return(
   <div>
-    <h1>I am prashansha</h1>
-     <h1>Inside app component</h1>
-     <Employee></Employee>\
-     <Demo></Demo>
+    <h1>user variable with jsx</h1>
+    <h2>Value of variable is{name}</h2>
+    <h3>{x+y}</h3>
+    <h4>{PP()}</h4>
+    <h5>example of paramiterized function {sum(x,y)}</h5>
   </div>
  )
 }
-function Employee(){
-  return(
-     <h1>the employee name is prashansha</h1>
-  )
-}
+
 export default App
